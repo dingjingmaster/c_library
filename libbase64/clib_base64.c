48f424e44bfca227a5a8263e9435c6597847e94c
dingjingmaster <dingjing@live.cn>
2019-06-24 18:28:45 +0800

    base64 加密xiuzhengbug

--- libbase64/clib_base64.c
+++ libbase64/clib_base64.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 /* 标准 base64 编码 */
-static char _base64_enc[64] = {
+static const char _base64_enc[64] = {
     'A','B','C','D','E','F','G','H','I','J','K','L','M',        /* 0  ~ 12 */
     'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',        /* 13 ~ 25 */
     'a','b','c','d','e','f','g','h','i','j','k','l','m',        /* 26 ~ 38 */
@@ -10,7 +10,7 @@ static char _base64_enc[64] = {
     '0','1','2','3','4','5','6','7','8','9','+','/'             /* 52 ~ 63 */
 };
 
-static char _base64_dec[256] = {
+static const uint8_t _base64_dec[256] = {
 
 };
 
@@ -23,9 +23,9 @@ size_t _base64_encode(char* target, const void* source, size_t bytes, const char
         target[j++] = table[(ptr[i] >> 2) & 0x3F];              /* 取前 6 位bit 高位置零 0x3F == 0x0011 1111 */
         target[j++] = table[((ptr[i] & 0x03) << 4)\
                 | ((ptr[i + 1] >> 4) & 0x0F)];                  /* 取第一个 8bit 的后两位 + 第二个 8 bit 的前四位 */
-        target[j++] = table[((ptr[i] & 0x0F) << 2)\
-                | ((ptr[i + 1] >> 6) & 0x03)];                  /* 取第二个 8bit 的后四位 + 第三个 8bit 的前 两位 */
-        target[j++] = table[(ptr[i] & 0x3F)];                   /* 取第三个 8bit 的后六位 */
+        target[j++] = table[((ptr[i + 1] & 0x0F) << 2)\
+                | ((ptr[i + 2] >> 6) & 0x03)];                  /* 取第二个 8bit 的后四位 + 第三个 8bit 的前 两位 */
+        target[j++] = table[(ptr[i + 2] & 0x3F)];               /* 取第三个 8bit 的后六位 */
     }
 
     /* 原串中不能被 3 整除的部分（有一个或者两个） */
@@ -46,4 +46,35 @@ size_t _base64_encode(char* target, const void* source, size_t bytes, const char
 
 size_t base64_encode(char* target, const void* source, size_t bytes) {
     return _base64_encode(target, source, bytes, _base64_enc);
+}
+
+size_t base64_decode(void* target, const char* source, size_t bytes) {
+    size_t i = 0, j = 0;
+    uint8_t* p = (uint8_t*)target;
+    const uint8_t* src = (const uint8_t*)source;
+    const uint8_t* end = NULL;
+
+    if(0 != bytes % 4) {
+        return (unsigned long)-1;
+    }
+    end = src + bytes;
+
+    /* 大于 4bit */
+    for(j = 0; j < bytes / 4; ++j) {
+        p[i++] = (_base64_dec[src[0]] << 2) | (_base64_dec[src[1]] >> 4);
+        p[i++] = (_base64_dec[src[1]] << 4) | (_base64_dec[src[2]] >> 2);
+        p[i++] = (_base64_dec[src[2]] << 6) | (_base64_dec[src[3]]);
+        src += 4;
+    }
+    /* 等于 4bit */
+    if(end == src + 4) {
+        p[i++] = (_base64_dec[src[0]] << 2) | (_base64_dec[src[1]] >> 4);
+        if('=' != src[2]) {
+            p[i++] = (_base64_dec[src[1]] << 4) | (_base64_dec[src[2]] >> 2);
+        }
+        if('=' != src[3]) {
+            p[i++] = (_base64_dec[src[2]] << 6) | (_base64_dec[src[3]]);
+        }
+    }
+    return i;
 }
--- libbase64/clib_base64_example.c
+++ libbase64/clib_base64_example.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <string.h>
+#include "clib_base64.h"
 
-int main()
-{
-    printf("Hello World!\n");
+int main() {
+    const char* str = "dasda!@#~dsvref4568%^*^7*^&9*&)_+g测试汉字";
+    char tar[1024] = {0};
+    size_t ret = base64_encode(tar, str, strlen(str));
+    printf("encode string length: %ld\n%s\n", ret, tar);
+
     return 0;
 }
