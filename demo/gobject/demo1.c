f9c4aea84b537793e156d28b2d784dc191217d5d
dingjingmaster <dingjing@live.cn>
2019-12-03 11:12:25 +0800

    gobject第一个类

--- demo/gobject/demo1.c
+++ demo/gobject/demo1.c
@@ -5,11 +5,25 @@
 > Created Time: 2019年12月02日 星期一 18时30分18秒
  ************************************************************************/
 
-#include <stdio.h>
-#include "demo1/myobject.c"
+#include "demo1/double-list.h"
 
 int main (int argc, char* argv)
 {
+    // GObject 库的类型管理系统的初始化
+    g_type_init ();
+
+    int i;
+    demo_list_t* l;
+
+    /* 三个实例化 */
+    for (i = 0; i < 3; ++ i) {
+        l = g_object_new (DEMO_TYPE_DLIST, NULL);
+        g_object_unref (l);
+    }
+
+    l = g_object_new (DEMO_TYPE_DLIST, NULL);
+    if (G_IS_OBJECT(l))
+        g_print ("this is a gobject object!\n");
 
     return 0;
 }
