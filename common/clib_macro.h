c1b9d0fc3922fcf2acb7e5b9ed4ec8898c837e03
dingjingmaster <dingjing@live.cn>
2020-04-26 11:59:34 +0800

    常用宏

---
+++ common/clib_macro.h
@@ -0,0 +1,66 @@
+/*************************************************************************
+> FileName: clib_macro.h
+> Author  : DingJing
+> Mail    : dingjing@live.cn
+> Created Time: 2020年04月26日 星期日 11时34分53秒
+ ************************************************************************/
+#ifndef _CLIB_MACRO_H
+#define _CLIB_MACRO_H
+
+/**
+ * 这里把平时常用的宏添加进来
+ * 删除要慎重!  (一般不可删除)
+ * 修改要慎重!  (要写测试用例)
+ * 添加要慎重!  (多于1条语句或很长的语句才添加到这里)
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* 释放 malloc 申请的内存 */
+#define CT_CFREE(pointer)                                           \
+    do {                                                            \
+        if (NULL != pointer) free (pointer)                         \
+    } while (0);
+
+/* 调用函数 */
+#define CT_CALL_FUNC(func, ...)                                     \
+    func(##__VA_ARGS__)                                             \
+
+/* 交换 */
+#define CT_SWAP(first, second, stype)                               \
+    do {                                                            \
+        stype tmp = first;                                          \
+        first = second;                                             \
+        second = tmp;                                               \
+    } while(0);
+
+#define CT_INT_MIN(a, b, min)                                       \
+        min = a > b ? b : a;
+
+#define CT_INT_MAX(a, b, max)                                       \
+        min = a > b ? a : b;
+
+/* 空指针返回 */
+#define CT_RETURN_IF_NULL(pointer, val)                             \
+    if (NULL == pointer) {                                          \
+        val == void ? return; : return val;                         \
+    }
+
+/* 失败返回 */
+#define CT_RETURN_IF_FAIL(fail, val)                                \
+    if (fail) {                                                     \
+        val == void ? return; : return val;                         \
+    }
+
+/* 成功返回 */
+#define CT_RETURN_IF_SUCCESS(success, val)                          \
+    if (success) {                                                  \
+        val == void ? return; : return val;                         \
+    }
+
+#ifdef __cplusplus
+}
+#endif
+#endif
