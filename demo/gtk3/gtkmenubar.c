ece49d0e2f271520a9c0786070eb011414d6be5e
dingjingmaster <dingjing@kylinos.cn>
2020-08-07 15:09:18 +0800

    gtkmenu

--- demo/gtk3/gtkmenubar.c
+++ demo/gtk3/gtkmenubar.c
@@ -43,5 +43,6 @@ int main( int argc, char *argv[])
     g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
     gtk_widget_show_all(window);
     gtk_main();
+
     return 0;
 }
