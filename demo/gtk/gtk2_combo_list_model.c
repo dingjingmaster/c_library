cfd29c882247ff03c323747f4c2cb62d460aa524
dingjingmaster <dingjing@kylinos.cn>
2020-08-08 09:17:01 +0800

    comboxbox 视图-模型

--- demo/gtk/gtk2_combo_list_model.c
+++ demo/gtk/gtk2_combo_list_model.c
@@ -15,7 +15,7 @@ enum
 
 GtkTreeModel* createModel()
 {
-    const gchar *files[] = {"caterpillar.jpg", "momor.jpg", "hamimi.jpg", "bush.jpg"};
+    const gchar *files[] = {"resource/1.png", "resource/2.png", "resource/3.png", "resource/4.png"};
     GdkPixbuf *pixbuf;
     GtkTreeIter iter;
     GtkListStore *store;
@@ -38,8 +38,9 @@ gboolean combo_changed(GtkComboBox *comboBox, GtkLabel *label)
     GtkTreeModel *model = gtk_combo_box_get_model(comboBox);
     GtkTreeIter iter;
     gchar *active;
+
     gtk_combo_box_get_active_iter(comboBox, &iter);
-    gtk_tree_model_get(model, &iter, 1, &active, -1);
+    gtk_tree_model_get(model, &iter, 1, &active, -1);   // 参数：model、激活的那一列、第几列、激活那一列的值、结束符号
 
     gtk_label_set_text(label, active);
 }
@@ -58,20 +59,27 @@ int main(int argc, char *argv[])
     gtk_window_set_title(GTK_WINDOW(window), "GtkComboBox");
     gtk_window_set_default_size(GTK_WINDOW(window), 200, 50);
 
+
+    // ComboBox 模型部分
     comboBox = gtk_combo_box_new_with_model(createModel());
     gtk_combo_box_set_active(GTK_COMBO_BOX(comboBox), 0);
+
     renderer = gtk_cell_renderer_pixbuf_new();
     gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(comboBox), renderer, FALSE);
     gtk_cell_layout_set_attributes(GTK_CELL_LAYOUT(comboBox), renderer, "pixbuf", PIXBUF_COL,  NULL);
+
     renderer = gtk_cell_renderer_text_new();
     gtk_cell_layout_pack_start(GTK_CELL_LAYOUT(comboBox), renderer, FALSE);
     gtk_cell_layout_set_attributes(GTK_CELL_LAYOUT(comboBox), renderer, "text", TEXT_COL, NULL);    
 
-    label = gtk_label_new("caterpillar.jpg");
+
+    // 主界面布局
+    label = gtk_label_new("resource/1.png");
     vbox = gtk_vbox_new(TRUE, 5);
 
     gtk_box_pack_start(GTK_BOX(vbox), comboBox, TRUE, TRUE, 5);
     gtk_box_pack_start(GTK_BOX(vbox), label, TRUE, TRUE, 5);
+
     gtk_container_add(GTK_CONTAINER(window), vbox);
 
     g_signal_connect(GTK_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
