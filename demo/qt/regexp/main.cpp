8bfebdd0f7931d3006111c707ac3629268328e7a
dingjingmaster <dingjing@live.cn>
2020-09-15 14:01:14 +0800

    change

--- demo/qt/regexp/main.cpp
+++ demo/qt/regexp/main.cpp
@@ -7,12 +7,15 @@ int main(int argc, char *argv[])
     QString str1 = "asdsad(1).txt";
     QString str2 = "bbbb(2)kkk(1)";
     QString str3 = "bbbb(2)kkk(1).txt";
+    QString str4 = "bbbb(2)kkk";
 
     QRegExp regx("\\(\\d+\\)(\\.[a-zA-Z]+|)$");
 
     qDebug() << regx.indexIn(str1);
     qDebug() << regx.indexIn(str2);
     qDebug() << regx.indexIn(str3);
+    qDebug() << regx.indexIn(str4);
+
 
     Q_UNUSED(argc)
     Q_UNUSED(argv)
