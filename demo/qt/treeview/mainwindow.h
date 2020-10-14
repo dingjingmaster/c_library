5770a60926efd0b577acce9bbf3b2e71b30dfdd9
dingjingmaster <dingjing@live.cn>
2020-10-14 11:24:04 +0800

    tree  view

---
+++ demo/qt/treeview/mainwindow.h
@@ -0,0 +1,25 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+class QTreeView; //forward declarations
+class QStandardItemModel;
+class QStandardItem;
+QT_END_NAMESPACE
+
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+private:
+    QTreeView *treeView;
+    QStandardItemModel *standardModel;
+    QList<QStandardItem *> prepareRow(const QString &first,
+                                      const QString &second,
+                                      const QString &third) const;
+public:
+    MainWindow(QWidget *parent = nullptr);
+};
+#endif // MAINWINDOW_H
