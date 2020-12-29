3352fc144fdfd5b12bc5e4a6996c1a84c5f004d8
dingjingmaster <dingjing@live.cn>
2020-12-29 20:00:30 +0800

    修改

--- linux-tools/warm_reboot.sh
+++ linux-tools/warm_reboot.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-rm -rf run.log Dmesg* Sys*
+workdir="$(dirname $(realpath -- $0))/reboot/"
+
+rm -rf "${workdir}/run.log" "${workdir}/Dmesg*" "${workdir}/Sys*"
 
 rootPWD="123123"
-logFile=`pwd`/run.log
-dmesg_logfile=`pwd`/Dmesg.log
-syslog_file=`pwd`/Syslog.log
+logFile="${workdir}/run.log"
+dmesg_logfile="${workdir}/Dmesg.log"
+syslog_file="${workdir}/Syslog.log"
 
 inputSTR[0]="#!/bin/bash"
-inputSTR[1]="source /opt/times.left"
-inputSTR[2]="rm -rf /opt/times.left"
+inputSTR[1]="source ${workdir}/times.left"
+inputSTR[2]="rm -rf ${workdir}/times.left"
 inputSTR[3]="if [ \${timesLeft} -gt '0' ];then"
 inputSTR[4]="	echo \"还剩余\$((timesLeft-1))次重启。\" >> ${logFile}"
 inputSTR[5]="   echo ${rootPWD} | sudo -S cat /var/log/syslog > ${dmesg_logfile}-\${timesLeft}"
@@ -17,18 +19,18 @@ inputSTR[6]="   echo ${rootPWD} | sudo -S dmesg > ${syslog_file}-\${timesLeft}"
 inputSTR[7]="   echo ${rootPWD} | sudo -S cat /dev/null > /var/log/syslog"
 inputSTR[8]="	date >> ${logFile}"
 inputSTR[9]="	echo >> ${logFile}"
-inputSTR[10]="	echo "timesLeft=\$\(\(timesLeft-1\)\)" > /opt/times.left"
+inputSTR[10]="	echo "timesLeft=\$\(\(timesLeft-1\)\)" > ${workdir}/times.left"
 inputSTR[11]="	reboot"
 inputSTR[12]="else"
 inputSTR[13]="	sed -i '/.\/rbt.sh/d' /etc/crontab"
-inputSTR[14]="	rm -rf /opt/rbt.sh"
+inputSTR[14]="	rm -rf ${workdir}/rbt.sh"
 inputSTR[15]="	chown ${USER}:${USER} ${logFile}"
 inputSTR[16]="fi"
 
 echo -n -e "请输入自动重启的次数:\t"
 read timesLeft
-echo "timesLeft=${timesLeft}" > `pwd`/times.left
-echo ${rootPWD} | sudo -S mv `pwd`/times.left /opt/times.left &> /dev/null
+echo "timesLeft=${timesLeft}" > ${workdir}/times.left
+echo ${rootPWD} | sudo -S mv ${workdir}/times.left ${workdir}/times.left &> /dev/null
 
 echo -n -e "请输入重启间隔的时间（分钟）:\t"
 read interval
@@ -38,11 +40,11 @@ echo -e "Checking necessary files for auto rebooting ..."
 if [ ! -f /opt/rbt.sh ];then
 	for ((i=0; i<${#inputSTR[@]}; i++))
 	do
-		echo ${inputSTR[$i]} >> `pwd`/rbt.sh
+		echo ${inputSTR[$i]} >> ${workdir}/rbt.sh
 	done
-	echo ${rootPWD} | sudo -S mv `pwd`/rbt.sh /opt/rbt.sh
-	echo ${rootPWD} | sudo -S chown root:root /opt/rbt.sh
-	echo ${rootPWD} | sudo -S chmod a+x /opt/rbt.sh
+	echo ${rootPWD} | sudo -S mv ${workdir}/rbt.sh ${workdir}/rbt.sh
+	echo ${rootPWD} | sudo -S chown root:root ${workdir}/rbt.sh
+	echo ${rootPWD} | sudo -S chmod a+x ${workdir}/rbt.sh
 fi
 echo -e "\e[1;32m[ REBOOT SCRIPT OK]\e[0m"
 
@@ -50,7 +52,7 @@ oldIFS=$IFS
 IFS=$'\n'
 cfgOK=$(cat /etc/crontab | grep "./rbt.sh")
 if [ ! ${cfgOK} ];then
-	echo ${rootPWD} | sudo -S sed -i "\$a\*\/${interval:=2} * * * * root cd \/opt && .\/rbt.sh" /etc/crontab
+	echo ${rootPWD} | sudo -S sed -i "\$a\*\/${interval:=2} * * * * root cd ${workdir} && .\/rbt.sh" /etc/crontab
 fi
 IFS=$oldIFS
 echo -e "\e[1;32m[CRONTAB CONFIG OK]\e[0m"
