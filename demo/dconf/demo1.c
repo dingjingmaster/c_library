761880dee45f1378ba98f2007422491e8e9ea285
dingjingmaster <dingjing@live.cn>
2021-01-21 11:03:38 +0800

    完整的代码

--- demo/dconf/demo1.c
+++ demo/dconf/demo1.c
@@ -12,35 +12,77 @@
 #define SYSTEM_PROXY_HTTPS  "org.gnome.system.proxy.https"
 
 void change_func        (GSettings* gs, char* key, gpointer udata);
-void change_func_http   (GSettings* gs, char* key, gpointer udata);
-void change_func_https  (GSettings* gs, char* key, gpointer udata);
+void change_mode        (GSettings* gs, char* key, gpointer udata);
 
 int main (int argc, char* argv[])
 {
+    gchar* type = NULL;
     gtk_init (&argc, &argv);
 
-    GSettings* gsetting = g_settings_new (SYSTEM_PROXY);
-    GSettings* gsetting_http = g_settings_new (SYSTEM_PROXY_HTTP);
-    GSettings* gsetting_https = g_settings_new (SYSTEM_PROXY_HTTPS);
+    GSettings* gsetting         = g_settings_new (SYSTEM_PROXY);
+    GSettings* gsetting_http    = g_settings_new (SYSTEM_PROXY_HTTP);
+    GSettings* gsetting_https   = g_settings_new (SYSTEM_PROXY_HTTPS);
     if (NULL == gsetting) {
         printf ("错误!\n");
         return -1;
     }
 
-    g_signal_connect (gsetting, "changed", change_func, NULL);
-    g_signal_connect (gsetting_http, "changed", change_func, NULL);
-    g_signal_connect (gsetting_https, "changed", change_func, NULL);
+    // init
+    type = g_settings_get_string (gsetting, "mode");
+    if (0 == g_strcmp0("none", type)) {
+        printf ("none\n");
+    } else if (0 == g_strcmp0("manual", type)) {
+        printf ("manual\n");
+    } else if (0 == g_strcmp0 ("auto", type)) {
+        printf ("auto\n");
+    }
+
+    g_signal_connect (gsetting,       "changed", change_mode, NULL);
+    g_signal_connect (gsetting_http,  "changed", change_func, "http");
+    g_signal_connect (gsetting_https, "changed", change_func, "https");
 
     gtk_main();
 
     return 0;
 }
 
+void change_mode (GSettings* gs, char* key, gpointer udata)
+{
+    char*       ip = NULL;
+    gint32      port = 0;
+
+    GVariant* val = g_settings_get_value (gs, key);
+
+    printf ("%s\n", g_variant_get_type(val));
+    if (0 == g_strcmp0 (key, "mode")) {
+        printf ("%s\n", g_variant_get_string(val, NULL));
+    }
+}
+
 void change_func (GSettings* gs, char* key, gpointer udata)
 {
+    char*       ip = NULL;
+    gint32      port = 0;
+
     GVariant* val = g_settings_get_value (gs, key);
     GVariantType* type = g_variant_get_type (val);
 
-    printf ("changed --- key:%s -- value:%s\n", key, g_variant_get_string (val, NULL));
+    printf ("%s\n", g_variant_get_type(val));
+
+    if (0 == g_strcmp0 (key, "host") || 0 == g_strcmp0 (key, "port")) {
+        if (g_variant_is_of_type (val, G_VARIANT_TYPE_INT32)) {
+            GVariant* key = g_settings_get_value (gs, "host");
+            printf ("host:%s --- port: %u\n", g_variant_get_string (key, NULL), g_variant_get_int32 (val));
+        } else if (g_variant_is_of_type (val, G_VARIANT_TYPE_STRING)) {
+            GVariant* key = g_settings_get_value (gs, "port");
+            printf ("host:%s --- port: %u\n", g_variant_get_string (val, NULL), g_variant_get_int32 (key));
+        }
+    }
+
+    if (0 == g_strcmp0 ("http", udata)) {
+        printf ("http\n");
+    } else if (0 == g_strcmp0 ("https", udata)) {
+        printf ("https\n");
+    }
 }
 
