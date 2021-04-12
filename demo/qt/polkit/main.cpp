25f62c625bf7d3e1b4a57a27016e743298fc5e10
dingjingmaster <dingjing@live.cn>
2021-04-12 16:33:32 +0800

    polkit

---
+++ demo/qt/polkit/main.cpp
@@ -0,0 +1,11 @@
+#include <QCoreApplication>
+#include <polkit-qt5-1/polkitqt1-authority.h>
+
+void
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    return a.exec();
+}
