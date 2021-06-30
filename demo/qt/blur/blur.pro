543464b788a1a12c9df325ff8b2f8ff2873cf776
dingjingmaster <dingjing@live.cn>
2021-06-30 10:04:19 +0800

    毛玻璃

--- demo/qt/blur/blur.pro
+++ demo/qt/blur/blur.pro
@@ -9,11 +9,9 @@ CONFIG += c++11
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-    main.cpp \
-    widget.cpp
+    main.cpp
 
-HEADERS += \
-    widget.h
+HEADERS +=
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
--- demo/qt/blur/main.cpp
+++ demo/qt/blur/main.cpp
@@ -1,11 +1,119 @@
-#include "widget.h"
+#include <QtGui>
+#if QT_VERSION >= QT_VERSION_CHECK(5,0,0)
+#include <QtWidgets>
+#endif
 
-#include <QApplication>
+class OverlayWidget : public QWidget {
+    void newParent() {
+        if (!parent()) return;
+        parent()->installEventFilter(this);
+        raise();
+    }
+public:
+    explicit OverlayWidget(QWidget *parent = {}) : QWidget(parent) {
+        setAttribute(Qt::WA_NoSystemBackground);
+        setAttribute(Qt::WA_TransparentForMouseEvents);
+        newParent();
+    }
+protected:
+    //! Catches resize and child events from the parent widget
+    bool eventFilter(QObject *obj, QEvent *ev) override {
+        if (obj == parent()) {
+            if (ev->type() == QEvent::Resize)
+                resize(static_cast<QResizeEvent*>(ev)->size());
+            else if (ev->type() == QEvent::ChildAdded)
+                raise();
+        }
+        return QWidget::eventFilter(obj, ev);
+    }
+    //! Tracks parent widget changes
+    bool event(QEvent *ev) override {
+        if (ev->type() == QEvent::ParentAboutToChange) {
+            if (parent()) parent()->removeEventFilter(this);
+        }
+        else if (ev->type() == QEvent::ParentChange)
+            newParent();
+        return QWidget::event(ev);
+    }
+};
+
+class ContainerWidget : public QWidget
+{
+public:
+    explicit ContainerWidget(QWidget *parent = {}) : QWidget(parent) {}
+    void setSize(QObject *obj) {
+        if (obj->isWidgetType()) static_cast<QWidget*>(obj)->setGeometry(rect());
+    }
+protected:
+    //! Resizes children to fill the extent of this widget
+    bool event(QEvent *ev) override {
+        if (ev->type() == QEvent::ChildAdded) {
+            setSize(static_cast<QChildEvent*>(ev)->child());
+        }
+        return QWidget::event(ev);
+    }
+    //! Keeps the children appropriately sized
+    void resizeEvent(QResizeEvent *) override {
+        for(auto obj : children()) setSize(obj);
+    }
+};
+
+class LoadingOverlay : public OverlayWidget
+{
+public:
+    LoadingOverlay(QWidget *parent = {}) : OverlayWidget{parent} {
+        setAttribute(Qt::WA_TranslucentBackground);
+    }
+protected:
+    void paintEvent(QPaintEvent *) override {
+        QPainter p{this};
+        p.fillRect(rect(), {100, 100, 100, 128});
+        p.setPen({200, 200, 255});
+        p.setFont({"arial,helvetica", 48});
+        p.drawText(rect(), "Loading...", Qt::AlignHCenter | Qt::AlignTop);
+    }
+};
+
+namespace compat {
+#if QT_VERSION >= QT_VERSION_CHECK(5,4,0)
+using QT_PREPEND_NAMESPACE(QTimer);
+#else
+using Q_QTimer = QT_PREPEND_NAMESPACE(QTimer);
+class QTimer : public Q_QTimer {
+public:
+    QTimer(QTimer *parent = nullptr) : Q_QTimer(parent) {}
+    template <typename F> static void singleShot(int period, F &&fun) {
+        struct Helper : public QObject {
+            F fun;
+            QBasicTimer timer;
+            void timerEvent(QTimerEvent *event) override {
+                if (event->timerId() != timer.timerId()) return;
+                fun();
+                deleteLater();
+            }
+            Helper(int period, F &&fun) : fun(std::forward<F>(fun)) {
+                timer.start(period, this);
+            }
+        };
+        new Helper(period, std::forward<F>(fun));
+    }
+};
+#endif
+}
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    Widget w;
-    w.show();
+    QApplication a{argc, argv};
+    ContainerWidget base;
+    QLabel label("Dewey, Cheatem and Howe, LLC.", &base);
+    label.setFont({"times,times new roman", 32});
+    label.setAlignment(Qt::AlignCenter);
+    label.setGraphicsEffect(new QGraphicsBlurEffect);
+    LoadingOverlay overlay(&base);
+    base.show();
+    compat::QTimer::singleShot(2000, [&]{
+        overlay.hide();
+        label.setGraphicsEffect({});
+    });
     return a.exec();
 }
